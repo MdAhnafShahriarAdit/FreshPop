0839c91c0e3182f71a4d4e561c2da0fa4965366a
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2020-02-28 21:19:02 +0600

    admin footer

---
+++ Admin/admin_footer.php
@@ -0,0 +1,18 @@
+<style>
+  .footer
+  {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  line-height: 40px;
+  color: white;
+  text-align:center; ;
+  background: linear-gradient(to right, #0066ff 20%, #660066 84%);
+  }
+</style>
+
+<div class="footer">
+   <p>FreshPop</p>
+   </div>
