a8acf87f127ac4ddc8e1891ee6e41ed085bda827
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2020-02-28 21:19:20 +0600

    admin home

---
+++ Admin/admin_home.php
@@ -0,0 +1,40 @@
+<?php include 'admin_header.php'; ?>
+<?php include 'admin_footer.php'; ?>
+<?php include 'snav.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin home</title>
+</head>
+<body>
+<div class="bg-text">Welcome To Admin Panel</div>
+</body>
+</html>
+<style>
+html,body{
+background-image: url('image/img4.png');
+background-size: cover;
+
+}
+.bg-text 
+{
+  background-color: rgba(0,0,0, 0.5); 
+  color: #A9A9A9;
+  font-weight: bold;
+  font-family: fantasy;
+  font-size: 90px;
+  border: 10px solid ;
+  border-color: darkblue;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 450px;
+  padding: 20px;
+  text-align: center;
+  animation-delay: 10s;
+}
+</style>
+
+
