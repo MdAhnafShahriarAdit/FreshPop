a0c29987d989dbb531492df383ea16661c89fb41
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2020-02-28 20:55:32 +0600

    changed in php

---
+++ movtest/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FreshPOP</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   
+
+</head>
+<body>
+ <div class="container header">
+ 	<img src="img/logo6.png" class="logo">
+ 	<a href="movie.html" class="header-btn "><i class="fa fa-user" aria-hidden="true"></i>
+Logout</a>
+ 	<a href="movie.html" class="header-btn "><i class="fa fa-user" ></i>About US</a>
+ 	<a href="movie.html" class="header-btn "><i class="fa fa-user" ></i>Contact US</a>
+ 	<a href="movie.html" class="header-btn "><i class="fa fa-user" ></i>Ratings</a>
+ 	<a href="movie.html" class="header-btn "><i class="fa fa-user" ></i>Be a Pro Member</a>
+ 	<a href="movie.html" class="header-btn "><i class="fa fa-user" ></i>Home</a>
+
+
+
+
+ </div>
+ <div class="container movie-details">
+ 	<div class="row">
+ 		<div class="col-md-6 left-box" >
+ 			<h1>FreshPOP</h1>
+ 			<p>The internet seems too full of negativity. We don’t believe in tearing anyone or any work of art down due to this. Comics journalism is a new form of journalism. Finally, we don’t come to write comic book reviews as experts. We arrive at writing them from the perspective of students of comics and comic book theory. <br> Please enjoy reading our reviews!</p>
+ 			<p>What we do </p>
+ 			<div class="casting   ">
+ 				<img style="height: 100% " src="img/f1.jpg"  title="Reviews"> 
+ 				<img style="height: 100% " src="img/f2.png" title="Watch Movies">
+ 				<img style="height: 100% " src="img/f3.jpg" title="Previews">
+ 				<img style="height: 100% " src="img/f4.jpg" title="Features">
+
+ 		
+ 			
+ 		</div>
+
+ 		<a href="#" class="down-btn"><i class="fa fa-play"></i>Watch Now In Pro version</a>
+ 		<a href="#" class="down-btn"><i class="fa fa-list-alt"></i> Watchlist</a>
+ 		</div>
+ 		<div class="col-md-6 text-center">
+ 			<img src="img/img2.jpg" class="movie-img">
+ 	</div>
+ </div>
+
+ <div class="container series">
+ 	<h3>Popular Movies</h3>
+     <div class="row">
+ 	 <div class="col-md-4">
+ 		<div class="row">
+ 			<div class="col-6 col-md-4"><img src="img/b9.jpg"></div>
+ 			<div class="col-6 col-md-4"><img src="img/b7.jpg"></div>
+ 		</div>
+ 	</div>
+ 	<div class="col-md-4">
+ 		<div class="row">
+ 			<div class="col-6 col-md-4"><img src="img/b8.jpg"></div>
+ 			<div class="col-6 col-md-4"><img src="img/b6.jpg"></div>
+ 		</div>
+ 		
+ 	</div>
+ 	<div class="col-md-4">
+ 		<div class="row">
+ 			<div class="col-6 col-md-4"><img src="img/m1.png"></div>
+ 			<div class="col-6 col-md-4"><img src="img/b7.jpg"></div>
+ 		</div>
+ 		
+ 	</div>
+ 	</div>
+ </div>
+</body>
+</html>
+
+
